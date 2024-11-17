6acd44aa929fa82f90668582faf03cf89fc7afe4
Arthurffreiree <ArthurDuarteFreire@gmail.com>
2024-11-17 19:31:30 -0300

    Finalização dos metodos dos clientes

---
+++ src/main/java/com/novohoteldb/controller/ClienteController.java
@@ -0,0 +1,63 @@
+package com.novohoteldb.controller;
+
+import com.novohoteldb.dto.CadastroDTO;
+import com.novohoteldb.dto.EnderecoDTO;
+import com.novohoteldb.dto.ReservaClienteRecepcionistaQuartoDTO;
+import com.novohoteldb.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/listar")
+    public List<Map<String, Object>> listarClientes(){
+        return clienteService.listarClientes();
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity cadastrarPessoa(@RequestBody CadastroDTO cadastro) {
+        return ResponseEntity.status(200).body(clienteService.cadastrarCliente(cadastro));
+    }
+
+    @PostMapping("/cadastro/endereco/{cpf}")
+    public ResponseEntity cadastrarEndereco(@PathVariable String cpf, @RequestBody EnderecoDTO enderecoDTO) {
+        return ResponseEntity.status(200).body(clienteService.cadastrarEndereco(cpf, enderecoDTO));
+    }
+
+    @DeleteMapping("/excluir/{cpf}")
+    public ResponseEntity excluirCliente(@PathVariable String cpf) {
+        try {
+            clienteService.excluirCliente(cpf);
+            return ResponseEntity.status(200).body("Cliente excluído com sucesso.");
+        } catch (RuntimeException e){
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/buscar/{cpf}")
+    public Map<String, Object> buscarCliente(@PathVariable String cpf) {
+        return clienteService.buscarCliente(cpf);
+    }
+
+    @PutMapping("/atualizar/{cpf}")
+    public ResponseEntity atualizarCliente(@PathVariable String cpf, @RequestBody CadastroDTO cadastro) {
+        clienteService.atualizarCliente(cpf, cadastro);
+        return ResponseEntity.status(200).body("Cliente atualizado com sucesso.");
+    }
+
+    @PutMapping("/atualizar/endereco/{cpf}")
+    public ResponseEntity atualizarEndereco(@PathVariable String cpf, @RequestBody EnderecoDTO endereco) {
+        clienteService.atualizarEndereco(cpf, endereco);
+        return ResponseEntity.status(200).body("Endereço atualizado com sucesso.");
+    }
+
+}
---
+++ src/main/java/com/novohoteldb/repository/ClienteRepository.java
@@ -0,0 +1,68 @@
+package com.novohoteldb.repository;
+
+import com.novohoteldb.dto.CadastroDTO;
+import com.novohoteldb.dto.EnderecoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ClienteRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Map<String, Object>> listarClientes() {
+        return jdbcTemplate.queryForList("select *\n" +
+                "from cliente c \n" +
+                "join pessoa p ON c.fk_Pessoa_CPF = p.CPF \n" +
+                "join endereco e on e.Endereco_PK = p.fk_Endereco_PK ");
+    }
+
+    public void cadastrarCliente(CadastroDTO cadastro) {
+        String sql =  "INSERT into PESSOA(cpf,nome,email,telefone1,telefone2) values(?,?,?,?,?)";
+        jdbcTemplate.update(sql,cadastro.cpf(),cadastro.nome(),cadastro.email(),cadastro.telefone1(),cadastro.telefone2());
+        jdbcTemplate.update("INSERT INTO CLIENTE (fk_Pessoa_CPF) values(?)",cadastro.cpf());
+    }
+
+    public void cadastrarEndereco(String cpf , EnderecoDTO endereco) {
+        String sql = "INSERT INTO ENDERECO(Endereco_PK, Rua, Numero, Bairro, Cep, fk_Pessoa_Cpf) values(?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, endereco.Endereco_PK(), endereco.Rua(), endereco.Numero(), endereco.Bairro(), endereco.Cep(), cpf);
+        String updatePessoa = "UPDATE PESSOA SET fk_Endereco_PK = ? WHERE CPF = ?";
+        jdbcTemplate.update(updatePessoa, endereco.Endereco_PK(), cpf);
+    }
+
+    public void excluirCliente(String cpf) {
+        String sql = "DELETE FROM PESSOA WHERE CPF = ?";
+        String sql2 = "DELETE FROM CLIENTE WHERE fk_Pessoa_CPF = ?";
+        String sql3 = "DELETE FROM ENDERECO WHERE fk_Pessoa_CPF = ?";
+
+        jdbcTemplate.update(sql2, cpf);
+        jdbcTemplate.update(sql3, cpf);
+        jdbcTemplate.update(sql, cpf);
+
+    }
+
+    public Map<String, Object> buscarCliente(String cpf) {
+        String sql = "select * from cliente c \n" +
+                "join pessoa p ON c.fk_Pessoa_CPF = p.CPF \n" +
+                "join endereco e on e.Endereco_PK = p.fk_Endereco_PK " +
+                "WHERE p.CPF = ?";
+        return jdbcTemplate.queryForMap(sql, cpf);
+
+    }
+
+    public void atualizarCliente(String cpf, CadastroDTO cadastro) {
+        String sql = "UPDATE PESSOA SET Email = ?, TELEFONE1 = ?, TELEFONE2 = ? WHERE CPF = ?";
+        jdbcTemplate.update(sql, cadastro.email(), cadastro.telefone1(), cadastro.telefone2(), cpf);
+    }
+
+    public void atualizarEndereco(String cpf, EnderecoDTO endereco) {
+        String sql = "UPDATE ENDERECO SET Rua = ?, Numero = ?, Bairro = ?, Cep = ? WHERE fk_Pessoa_CPF = ?";
+        jdbcTemplate.update(sql, endereco.Rua(), endereco.Numero(), endereco.Bairro(), endereco.Cep(), cpf);
+    }
+
+}
