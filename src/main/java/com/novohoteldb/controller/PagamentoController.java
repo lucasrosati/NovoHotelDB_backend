48b66eefd51a4e45ded6e20c917c209cdcd49d68
Arthurffreiree <ArthurDuarteFreire@gmail.com>
2024-11-17 17:09:01 -0300

    Atualização dos imports

--- src/main/java/com/novohoteldb/controller/PagamentoController.java
+++ src/main/java/com/novohoteldb/controller/PagamentoController.java
@@ -1,8 +1,11 @@
 package com.novohoteldb.controller;
 
-import com.novohoteldb.dto.ReservaClienteRecepcionistaQuartoDTO;
 import com.novohoteldb.service.PagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
